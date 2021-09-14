2d29f47453e04339d71c45c80d489201c25a4b8b
thev21 <1248232146@qq.com>
2021-09-14 16:53:48 +0800

    提交

---
+++ src/views/About.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="contain" style="height:100vh">
+    <img src="../assets/images/loginbg.png" alt="" style="height:100vh">
+    <div class="login">
+      登录框
+    </div>
+  </div>
+</template>
+<style scoped>
+
+.contain{
+  display: flex;
+  align-content: center;
+ 
+}
+.login{
+  
+}
+</style>
