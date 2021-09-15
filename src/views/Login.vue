d238827c233a500a2b3852ca563f008aa11de9b4
thev21 <1248232146@qq.com>
2021-09-15 10:54:27 +0800

    登录注册界面添加交互

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,69 +1,100 @@
-
 <template>
-  <div class="container">
-    <img src="../assets/img/bg.png" alt="" class="wave">
-    <div class="img">
-      <img src="../assets/img/img-4.svg" alt="">
-    </div>
-    <div class="login-box">
-      <form action="">
-        <img src="../assets/img/avatar.svg" alt="" class="avatar">
-        <h2 id="wel">Welcome</h2>
-        <div class="input-group">
-          <div class="icon">
-            <i class="iconfont icon-fa-user"></i>
+  <div>
+    <img src="../assets/img/bg.png" alt="" class="wave" />
+    <div class="container">
+      <div class="img">
+        <img src="../assets/img/img-4.svg" alt="" />
+      </div>
+      <div class="login-box">
+        <form action="">
+          <img src="../assets/img/avatar.svg" alt="" class="avatar" />
+          <h2>Welcome</h2>
+          <div class="input-group">
+            <div class="icon">
+              <i class="iconfont icon-fa-user"></i>
+            </div>
+            <div>
+              <h5>用户名</h5>
+              <input type="text" class="input" id="username" />
+            </div>
           </div>
-          <div>
-            <input type="text" class="input" id="username" placeholder="用户名">
+          <div class="input-group">
+            <div class="icon">
+              <i class="iconfont icon-fasfa-lockCopy"></i>
+            </div>
+            <div>
+              <h5>密码</h5>
+              <input type="password" class="input" id="password" />
+            </div>
           </div>
-        </div>
-        <div class="input-group">
-          <div class="icon">
-            <i class="iconfont icon-fasfa-lockCopy"></i>
-          </div>
-          <div>
-            <input type="password" class="input" id="password" placeholder="密码">
-          </div>
-        </div>
-        <a href="signup"><h3>Sign UP</h3></a><br>
-        <input type="button" @click="submit" class="btn" value="登录" id="btn_log">
-
-      </form>
+          <a href="signup">Sign UP</a>
+          <input
+            type="submit"
+            @click="submit"
+            class="btn"
+            value="登录"
+            id="btn_log"
+          />
+        </form>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
-import '@/assets/css/iconfont.css'
-import '@/assets/css/style.css'
-import axios from 'axios'
+import "@/assets/css/iconfont.css";
+import "@/assets/css/style.css";
+
+import axios from "axios";
 
 export default {
   name: "Login",
-  methods:{
-      submit:function () {
-      const username=document.getElementById('username').value
-      const password=document.getElementById('password').value
-      if(username===''||password===''){
-        alert('用户名和密码不能为空')
-      }else {
-        axios.post('http://localhost:3000/login',{
-          username:username,
-          password:password
-        }).then(function (res) {
-          if (res.data.flag){
-            location.href='../';
-          }else {
-            alert(res.data.msg)
-          }
-        }).catch(function (error) {
-          console.log(error)
-        })
-      }
-  }
-    }
-  }
-</script>
-<style scoped>
+  mounted: function() {
+    this.$nextTick(function() {
+      const inputs = document.querySelectorAll(".input");
 
-</style>
+      function focusFunction() {
+        let parentNode = this.parentNode.parentNode;
+        parentNode.classList.add("focus");
+      }
+      function blurFunction() {
+        let parentNode = this.parentNode.parentNode;
+        if (this.value == "") {
+          parentNode.classList.remove("focus");
+        }
+      }
+
+      inputs.forEach((input) => {
+        input.addEventListener("focus", focusFunction);
+        input.addEventListener("blur", blurFunction);
+      });
+    });
+  },
+  methods: {
+    submit: function() {
+      const username = document.getElementById("username").value;
+      const password = document.getElementById("password").value;
+      if (username === "" || password === "") {
+        alert("用户名和密码不能为空");
+      } else {
+        axios
+          .post("http://localhost:3000/login", {
+            username: username,
+            password: password,
+          })
+          .then(function(res) {
+            if (res.data.flag) {
+              location.href = "../";
+            } else {
+              alert(res.data.msg);
+            }
+          })
+          .catch(function(error) {
+            console.log(error);
+          });
+      }
+    },
+  },
+};
+</script>
+<style lang="scss" scoped></style>
--- src/views/Signup.vue
+++ src/views/Signup.vue
@@ -1,92 +1,126 @@
 <template>
-  <div class="container">
-    <img src="../assets/img/bg.png" alt="" class="wave">
-    <div class="img">
-      <img src="../assets/img/img-4.svg" alt="">
-    </div>
-    <div class="login-box">
-      <form action="">
-        <img src="../assets/img/avatar.svg" alt="" class="avatar">
-        <h2 id="wel">Welcome</h2>
-        <div class="input-group">
-          <div class="icon">
-            <i class="iconfont icon-fa-user"></i>
+  <div>
+    <img src="../assets/img/bg.png" alt="" class="wave" />
+    <div class="container">
+      <div class="img">
+        <img src="../assets/img/img-4.svg" alt="" />
+      </div>
+      <div class="login-box">
+        <form action="">
+          <img src="../assets/img/avatar.svg" alt="" class="avatar" />
+          <h2>Welcome</h2>
+          <div class="input-group">
+            <div class="icon">
+              <i class="iconfont icon-fa-user"></i>
+            </div>
+            <div>
+              <h5>用户名</h5>
+              <input type="text" class="input" id="username" />
+            </div>
           </div>
-          <div>
-            <input type="text" class="input" id="username" placeholder="用户名">
-          </div>
-        </div>
 
-        <div class="input-group">
-          <div class="icon">
-            <i class="iconfont icon-fasfa-lockCopy"></i>
+          <div class="input-group">
+            <div class="icon">
+              <i class="iconfont icon-fasfa-lockCopy"></i>
+            </div>
+            <div>
+              <h5>密码</h5>
+              <input type="password" class="input" id="password" />
+            </div>
           </div>
-          <div>
-            <input type="password" class="input" id="password" placeholder="密码">
-          </div>
-        </div>
 
-        <div class="input-group">
-          <div class="icon">
-            <i class="iconfont icon-shenfen1"></i>
+          <div class="input-group">
+            <div class="icon">
+              <i class="iconfont icon-shenfen1"></i>
+            </div>
+            <div>
+              <h5>角色</h5>
+              <input type="text" class="input" id="role"/>
+            </div>
           </div>
-          <div>
-            <input type="text" class="input" id="role" placeholder="身份">
-          </div>
-        </div>
 
-        <div class="input-group">
-          <div class="icon">
-            <i class="iconfont icon-dianhua"></i>
+          <div class="input-group">
+            <div class="icon">
+              <i class="iconfont icon-dianhua"></i>
+            </div>
+            <div>
+              <h5>手机号码</h5>
+              <input type="text" class="input" id="tel" />
+            </div>
           </div>
-          <div>
-            <input type="text" class="input" id="tel" placeholder="手机号码">
-          </div>
-        </div>
 
-        <a href="login"><h3>Log in</h3></a><br>
-        <input type="button" class="btn" @click="submit" value="注册" id="btn_reg">
-      </form>
+          <a href="login">Log in</a>
+          <input
+            type="button"
+            class="btn"
+            @click="submit"
+            value="注册"
+            id="btn_reg"
+          />
+        </form>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
-import '@/assets/css/iconfont.css'
-import '@/assets/css/style.css'
-import axios  from "axios";
+import "@/assets/css/iconfont.css";
+import "@/assets/css/style.css";
+import axios from "axios";
 
 export default {
-name: "Signup",
-  methods:{
-    submit:function () {
-      const username=document.getElementById('username').value
-      const password=document.getElementById('password').value
-      const role=document.getElementById('role').value
-      const tel=document.getElementById('tel').value
-      if(username===''||password===''||role===''||tel===''){
-        alert('请把信息填写完整~')
-      }else {
-        axios.post('http://localhost:3000/signup',{
-          username:username,
-          password:password,
-          role:role,
-          tel:tel
-        }).then(function (res) {
-          if (res.data.flag){
-            location.href='../login'
-          }else {
-            alert(res.data.msg)
-          }
-        }).catch(function (error) {
-          console.log(error)
-        })
+  name: "Signup",
+  mounted: function() {
+    this.$nextTick(function() {
+      const inputs = document.querySelectorAll(".input");
+
+      function focusFunction() {
+        let parentNode = this.parentNode.parentNode;
+        parentNode.classList.add("focus");
       }
-    }
-  }
-}
+      function blurFunction() {
+        let parentNode = this.parentNode.parentNode;
+        if (this.value == "") {
+          parentNode.classList.remove("focus");
+        }
+      }
+
+      inputs.forEach((input) => {
+        input.addEventListener("focus", focusFunction);
+        input.addEventListener("blur", blurFunction);
+      });
+    });
+  },
+  methods: {
+    submit: function() {
+      const username = document.getElementById("username").value;
+      const password = document.getElementById("password").value;
+      const role = document.getElementById("role").value;
+      const tel = document.getElementById("tel").value;
+      if (username === "" || password === "" || role === "" || tel === "") {
+        alert("请把信息填写完整~");
+      } else {
+        axios
+          .post("http://localhost:3000/signup", {
+            username: username,
+            password: password,
+            role: role,
+            tel: tel,
+          })
+          .then(function(res) {
+            if (res.data.flag) {
+              location.href = "../login";
+            } else {
+              alert(res.data.msg);
+            }
+          })
+          .catch(function(error) {
+            console.log(error);
+          });
+      }
+    },
+  },
+};
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
